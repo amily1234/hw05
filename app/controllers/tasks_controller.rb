c4c86314f6d4fc80ad415730b2382d12043e22ff
amily.wu <amily.wu@5xruby.com>
2022-03-31 22:49:13 +0800

    Feature/Step16_add_priority

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :content, :end_time, :status_type)
+    params.require(:task).permit(:title, :content, :end_time, :status_type, :priority_type)
   end
 
   def find_task
---
+++ db/migrate/20220331103753_add_priority_type_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddPriorityTypeToTasks < ActiveRecord::Migration[6.1]
+  def change
+    add_column :tasks, :priority_type, :string
+  end
+end
