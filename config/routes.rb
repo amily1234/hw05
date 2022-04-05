b98fbc361476fdefe346b8ec8496b6fe9eb5b49c
amily.wu <amily.wu@5xruby.com>
2022-04-06 00:11:04 +0800

    Feature/Step24_errors_page

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
     resources :users
     root to: 'tasks#index'
   end
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
