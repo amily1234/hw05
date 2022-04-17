3f5f283f2b1ae03879c6e404742fbe7aae66ece7
amily.wu <amily.wu@5xruby.com>
2022-04-17 11:57:46 +0800

    Feature/Step20_sign_up_and_sign_in-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,33 +2,36 @@ class TasksController < ApplicationController
   before_action :find_task, only: [:edit, :update, :destroy]
 
   def index
-    @tasks = Task.order(params[:sort])
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
+    @tasks = Current.user.tasks.order(params[:sort])
       if params[:title] || params[:status]
-        @tasks = Task.search_title_or_content( "%#{params[:title]}%" )			
+        @tasks = Current.user.tasks.search_title_or_content( "%#{params[:title]}%" )			
         .search_status_type( "%#{params[:status]}%" )
         .order_by_created_at
       elsif params[:sort]
-        @tasks = Task.order(params[:sort])
-        @tasks = Task.order(params[:sort]).page(params[:page]).per(3)
+        @tasks = Current.user.tasks.order(params[:sort])
+        @tasks = Current.user.tasks.order(params[:sort]).page(params[:page]).per(3)
       else
-        @tasks = Task.order_by_created_at
-        @tasks = Task.order_by_created_at.page(params[:page]).per(3)
+        @tasks = Current.user.tasks.order_by_created_at
+        @tasks = Current.user.tasks.order_by_created_at.page(params[:page]).per(3)
     end
   end
 
   def show
-    @tasks = Task.find(params[:id])
+    @tasks = Current.user.tasks.find(params[:id])
     render plain: "OK"
   end
 
   def new
-    @task = Task.new
+    @task = Current.user.tasks.new
   end
 
   def edit; end
     
   def create
-    @task = Task.new(task_params)
+    @task = Current.user.tasks.new(task_params)
     
     if @task.save
       redirect_to tasks_path, notice: I18n.t(:task_add_success_template)
@@ -39,9 +42,9 @@ class TasksController < ApplicationController
     
   def update
     if @task.update(task_params)
-        redirect_to tasks_path, notice: I18n.t(:task_update_success_template)
+      redirect_to tasks_path, notice: I18n.t(:task_update_success_template)
     else
-        render :edit
+      render :edit
     end
   end
 
@@ -56,6 +59,6 @@ class TasksController < ApplicationController
   end
 
   def find_task
-    @task = Task.find_by(id: params[:id])
+    @task = Current.user.tasks.find_by(id: params[:id])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def index
     @users = User.includes(:tasks)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: I18n.t(:create_account_success_template)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)      
+  end
 end
